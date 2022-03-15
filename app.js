39a252c357d07608b3ac5a1eb7a4190ca6ff3ff7
Phelipe <88689679+Phelipe353@users.noreply.github.com>
2022-03-15 09:14:29 -0300

    update

--- app.js
+++ app.js
@@ -13,6 +13,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization',
+  );
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  } else {
+    next();
+  }
+});
+
+/* app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET');
   app.use(cors());
@@ -26,7 +42,7 @@ app.use((req, res, next) => {
     return res.status(200).end();
   }
   next();
-});
+}); */
 
 app.use('/users', rotaUsers);
 app.use('/items', rotaItems);

33553fd5702eb38ffb2448db4f0a82771bfb0573
Phelipe <88689679+Phelipe353@users.noreply.github.com>
2022-03-15 13:46:33 -0300

    update

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization',
+    'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token, Authorization',
   );
 
   if (req.method === 'OPTIONS') {
